446b8cc8f3e8c932c32b29e89ced152b71761b5b
kentmoore1234 <kentmoore15@gmail.com>
2019-08-09 10:34:25 +0800

    added office

--- Ticketing System/ClientApp/src/app/app-routing.module.ts
+++ Ticketing System/ClientApp/src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { CounterComponent } from './views/counter/counter.component';
 import { FetchDataComponent } from './views/fetch-data/fetch-data.component';
-import { OfficeComponent } from './views/office/office.component';
+import { EmployeeComponent } from './views/employee/employee.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full'},
   { path: 'counter', component: CounterComponent},
   { path: 'fetch-data', component: FetchDataComponent},
-  { path: 'office', component: OfficeComponent},
+  { path: 'employee', component: EmployeeComponent},
   
 ]
 
--- Ticketing System/ClientApp/src/app/app.module.ts
+++ Ticketing System/ClientApp/src/app/app.module.ts
@@ -10,17 +10,10 @@ import { HomeComponent } from './views/home/home.component';
 import { FetchDataComponent } from './views/fetch-data/fetch-data.component';
 import { CounterComponent } from './views/counter/counter.component';
 import { NavMenuComponent } from './views/layouts/nav-menu/nav-menu.component';
-<<<<<<< HEAD
 import { EmployeeComponent } from './views/employee/employee.component';
 import { EmployeeAddFormComponent } from './views/employee/employee-add-form/employee-add-form.component';
 import { EmployeeUpdateFormComponent } from './views/employee/employee-update-form/employee-update-form.component';
 
-=======
-import { OfficeComponent } from './views/office/office.component';
-import { OfficeAddFormComponent } from './views/office/office-add-form/office-add-form.component';
-import { OfficeUpdateFormComponent } from './views/office/office-update-form/office-update-form.component';
-import { MatDialogModule } from '@angular/material';
->>>>>>> 6d2d6532e882551e73784511ee1ca6b02e942cb4
 
 @NgModule({
   declarations: [
@@ -29,15 +22,9 @@ import { MatDialogModule } from '@angular/material';
     FetchDataComponent,
     CounterComponent,
     NavMenuComponent,
-<<<<<<< HEAD
     EmployeeComponent,
     EmployeeAddFormComponent,
     EmployeeUpdateFormComponent
-=======
-    OfficeComponent,
-    OfficeAddFormComponent,
-    OfficeUpdateFormComponent
->>>>>>> 6d2d6532e882551e73784511ee1ca6b02e942cb4
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -46,9 +33,8 @@ import { MatDialogModule } from '@angular/material';
     ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDialogModule,
   ],
-  entryComponents:[OfficeUpdateFormComponent],
+  entryComponents:[],
   providers: [],
   bootstrap: [AppComponent]
 })
